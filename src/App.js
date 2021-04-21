4a9c151e4da3d93ad877eb1365352f3a0fc0757e
Artem Smirnov <aratatem2013@gmail>
2021-04-21 16:01:07 +0300

    тестовий комміт

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+// import React from "react";
 
-import user from "./user.json";
-import statisticalData from "./statistical-data.json";
-import friends from "./friends.json";
-import transactions from "./transactions.json";
+import user from './user.json';
+import statisticalData from './statistical-data.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
-import Profile from "./components/Profile";
-import Statistics from "./components/Statistics";
-import FriendList from "./components/FriendList";
-import TransactionHistory from "./components/TransactionHistory";
+import Profile from './components/Profile';
+import Statistics from './components/Statistics';
+import FriendList from './components/FriendList';
+import TransactionHistory from './components/TransactionHistory';
 
 const App = () => (
   <div>
-    <Profile name={user.name} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
     <Statistics title="Upload stats" stats={statisticalData} />
     {/* <Statistics stats={statisticalData} /> */}
     <FriendList friends={friends} />
