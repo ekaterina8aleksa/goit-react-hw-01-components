37d021c63f37fb09066203c24fdffce5e132a836
Artem Smirnov <aratatem2013@gmail>
2021-04-22 13:37:19 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 
 import user from './user.json';
 import statisticalData from './statistical-data.json';
@@ -23,7 +23,7 @@ const App = () => (
     <Statistics title="Upload stats" stats={statisticalData} />
     {/* <Statistics stats={statisticalData} /> */}
     <FriendList friends={friends} />
-    <TransactionHistory items={transactions} />
+    <TransactionHistory items={transactions111} />
   </Container>
 );
 
